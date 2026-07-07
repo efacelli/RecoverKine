cd240789fb4a9a6cbda01d93de7d028a2ce6893c
efacelli <enzomagic32@gmail.com>
2026-07-07 11:03:52 -0300

    url fix

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://recover-kine.vercel.app',
     credentials: true,
   })
 );
